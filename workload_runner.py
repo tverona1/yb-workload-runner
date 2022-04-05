7e18ae3b1187b03aec39b5c07ed1ed1f08eff262
Tomer Verona <tverona@yugabyte.com>
2022-04-05 13:17:09 -0700

    Add idle connection workload, timings, csv logging

--- workload_runner.py
+++ workload_runner.py
@@ -3,7 +3,8 @@ import logging
 from common.ybconnection import YBConnection
 from common.cluster_helper import ClusterHelper
 from common.executor import Executor
-from simple_workload import SimpleTenantApp
+from simple_workload import SimpleWorkload
+from idle_connections_workload import IdleConnectionsWorkload
 
 class Main:
     # Example usage (see below for overrides):
@@ -11,8 +12,12 @@ class Main:
     # Execute: python3 workload_runner.py execute --num_users 1000 --spawn_rate 20 --execution_time 600
     # Clean up: python3 workload_runner.py cleanup
 
+    def __init__(self):
+        # List of available workloads
+        self.workloads = {'idle_connections': IdleConnectionsWorkload, 'simple' : SimpleWorkload}
+
     def parse_arguments(self):
-        parser = argparse.ArgumentParser(description='Metadata Scalability')
+        parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         parser.add_argument('--host', default=None,
                             help="Host (default to localhost if not specified)")
         parser.add_argument('--port', default=None,
@@ -30,6 +35,7 @@ class Main:
         subparsers = parser.add_subparsers(dest='command')
 
         parser_setup_cluster = subparsers.add_parser('setup',
+                            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                             help="Setup cluster by creating databases and tables")
         parser_setup_cluster.add_argument('--num_databases', required=True,
                             type=int,
@@ -42,7 +48,11 @@ class Main:
                             help="Cleanup cluster")
 
         parser_execute_workload = subparsers.add_parser('execute',
+                            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                             help="Execute the workload")
+        parser_execute_workload.add_argument('--workload',
+                            choices=self.workloads.keys(), required=True,
+                            help="Workload to run")
         parser_execute_workload.add_argument('--num_users', default=100,
                             type=int,
                             help="Number of users")
@@ -52,6 +62,8 @@ class Main:
         parser_execute_workload.add_argument('--execution_time', default=600,
                             type=int,
                             help="Execution time in secs")
+        parser_execute_workload.add_argument('--csv', default=None,
+                            help="CSV file base name")
 
         args = parser.parse_args()
 
@@ -78,7 +90,7 @@ class Main:
         elif (args.command == 'cleanup'):
             cluster_helper.clean_cluster()
         elif (args.command == 'execute'):
-            workload_runner = Executor(cluster_helper, [SimpleTenantApp])
+            workload_runner = Executor(cluster_helper, [self.workloads[args.workload]], args.csv)
             workload_runner.execute(args.num_users, args.spawn_rate, args.execution_time)
         else:
             raise Exception(f'Unknown command {args.command}')
